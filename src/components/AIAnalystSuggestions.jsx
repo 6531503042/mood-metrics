6a06220310dfa6025cbf5a3016bf338c20d9b673
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-10-27 21:03:38 +0700

    Update AIAnalystSuggestions.jsx

--- src/components/AIAnalystSuggestions.jsx
+++ src/components/AIAnalystSuggestions.jsx
@@ -13,7 +13,7 @@ const backgroundShift = keyframes`
 const StyledCard = styled(Card)`
   position: relative;
   overflow: hidden;
-  background: linear-gradient(40deg, #dff9fb, #ffffff);
+  background: linear-gradient(40deg, #C6EBEDFF, #F6CDCDFF, #DAFFE4FF);x
   background-size: 200% 200%;
   animation: ${backgroundShift} 15s ease infinite;
 `;
