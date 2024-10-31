c341e64e20ce72b1dcf2333746dc77c116262255
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-10-31 13:10:14 +0700

    Remove: Unused component.

--- src/pages/DashboardOverview.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import FeedbackDashboard from '../components/FeedbackDashboard';
-
-const DashboardOverview = () => {
-  return <FeedbackDashboard />;
-};
-
-export default DashboardOverview;
