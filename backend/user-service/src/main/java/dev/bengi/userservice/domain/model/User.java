1c00c3d007c2f813baa0b33556009539514a3508
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-10-31 16:22:39 +0700

    User-service: Initial service.

---
+++ backend/user-service/src/main/java/dev/bengi/userservice/domain/model/User.java
@@ -0,0 +1,29 @@
+package dev.bengi.userservice.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
+
+@Entity(name = "users")
+@Table
+public class User {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    private UUID id;
+
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String email;
+
+
+}
