44367763b269e01438e8eae0c4d98eb7d4e24b6f
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-10-31 21:23:46 +0700

    User-service: Updated main runner.

--- backend/user-service/src/main/java/dev/bengi/userservice/UserServiceApplication.java
+++ backend/user-service/src/main/java/dev/bengi/userservice/UserServiceApplication.java
@@ -3,7 +3,12 @@ package dev.bengi.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+    scanBasePackages = {
+        "dev.bengi.userservice",
+        "dev.bengi.userservice.domain.model"
+    }
+)
 public class UserServiceApplication {
 
     public static void main(String[] args) {
