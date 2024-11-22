e322bb49c45db93ab4f6a2e378bfc02087914001
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-11-23 00:14:08 +0700

    User-Service: Adjust presentation dto layer.

---
+++ backend/user-service/src/main/java/dev/bengi/userservice/presentation/dto/response/AuthenticationResponse.java
@@ -0,0 +1,33 @@
+package dev.bengi.userservice.presentation.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    @JsonProperty("user_id")
+    private UUID userId;
+
+    @JsonProperty("email")
+    private String email;
+
+    @JsonProperty("first_name")
+    private String firstName;
+
+    @JsonProperty("last_name")
+    private String lastName;
+
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+}
