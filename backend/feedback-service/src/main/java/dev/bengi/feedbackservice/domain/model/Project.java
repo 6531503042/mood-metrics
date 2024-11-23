49d333cf622ae3cac0521293490e013c05fc7ef7
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-11-23 15:24:30 +0700

    Feedback-Service: Updated Doamin layer.

--- backend/feedback-service/src/main/java/dev/bengi/feedbackservice/domain/model/Project.java
+++ backend/feedback-service/src/main/java/dev/bengi/feedbackservice/domain/model/Project.java
@@ -1,20 +1,33 @@
 package dev.bengi.feedbackservice.domain.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Value;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
-@Value
+@Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "projects")
 public class Project {
-    UUID id;
-    String name;
-    String description;
-    LocalDateTime feedbackStartDate;
-    LocalDateTime feedbackEndDate;
-    List<Question> questions;
-    boolean isActive;
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+    private String description;
+    private LocalDateTime feedbackStartDate;
+    private LocalDateTime feedbackEndDate;
+    private boolean active;
+
+    @ManyToMany
+    @JoinTable(name = "project_questions", joinColumns = @JoinColumn(name = "project_id"), inverseJoinColumns = @JoinColumn(name = "question_id"))
+    private List<Question> questions;
 }
--- backend/feedback-service/src/main/java/dev/bengi/feedbackservice/domain/model/Question.java
+++ backend/feedback-service/src/main/java/dev/bengi/feedbackservice/domain/model/Question.java
@@ -1,18 +1,41 @@
 package dev.bengi.feedbackservice.domain.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Value;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.util.UUID;
 import java.util.List;
+import java.util.UUID;
 
-@Value
+@Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "questions")
 public class Question {
-    UUID id;
-    String content;
-    QuestionCategory category;
-    QuestionType type;
-    List<String> options;
-    boolean required;
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    private QuestionCategory category;
+
+    @Enumerated(EnumType.STRING)
+    private QuestionType type;
+
+    @ElementCollection
+    @CollectionTable(name = "question_options", joinColumns = @JoinColumn(name = "question_id"))
+    @Column(name = "option")
+    private List<String> options;
+
+    @Builder.Default
+    private boolean required = false;
+
+    @ManyToMany(mappedBy = "questions")
+    private List<Project> projects;
 }
