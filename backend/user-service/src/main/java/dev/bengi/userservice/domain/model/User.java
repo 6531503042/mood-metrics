485daf6a24561e80ddb94491b52f2ee9fdce4446
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-11-23 15:26:48 +0700

    User-Service: Updated domain layer.

--- backend/user-service/src/main/java/dev/bengi/userservice/domain/model/User.java
+++ backend/user-service/src/main/java/dev/bengi/userservice/domain/model/User.java
@@ -1,7 +1,10 @@
 package dev.bengi.userservice.domain.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,13 +15,27 @@ import java.util.UUID;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
 public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
     private String firstName;
     private String lastName;
+
+    @Column(unique = true)
     private String email;
     private String password;
+
+    @Enumerated(EnumType.STRING)
     private Role role;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Token> tokens;
 
     @Builder.Default
     private boolean enabled = true;
